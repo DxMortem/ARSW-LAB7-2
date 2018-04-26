ea2c2a2d43fe7c2c4a3ec51deabed1aa4ae6945d
Diego Borrero <die.9511@gmail.com>
2018-04-26 18:05:44 -0500

    Part 2.4 finished

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -6,8 +6,13 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
+import util.JedisUtil;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -19,20 +24,32 @@ public class STOMPMessagesHandler {
     @Autowired
     SimpMessagingTemplate msgt;
 
-    private ConcurrentHashMap<String,ArrayList<Point>> poligons = new ConcurrentHashMap<String, ArrayList<Point>>();
+
+
+    String luaScript = "local xval,yval; if (redis.call('LLEN','x')==4) then xval=redis.call('LRANGE','x',0,-1); yval=redis.call('LRANGE','y',0,-1); redis.call('DEL','x'); redis.call('DEL','y'); return {xval,yval}; else return {}; end";
 
     @MessageMapping("/newpoint.{numdibujo}")
     public void handlePointEvent(Point pt,@DestinationVariable String numdibujo) throws Exception {
-        System.out.println("Nuevo punto recibido en el servidor!:"+pt);
-        if (poligons.containsKey(numdibujo)){
-            poligons.get(numdibujo).add(pt);
-            if(poligons.get(numdibujo).size()>3){
-                msgt.convertAndSend("/topic/newpolygon."+numdibujo, poligons.get(numdibujo));
-            }
-        }else{
-            poligons.put(numdibujo,new ArrayList<Point>());
-            poligons.get(numdibujo).add(pt);
+        Jedis jedis = JedisUtil.getPool().getResource();
+        List<Object> res = new ArrayList<Object>();
+        Response<Object> luares = null;
+        while (!res.isEmpty()){
+            jedis.watch("x","y");
+            Transaction t = jedis.multi();
+            t.rpush("x", String.valueOf(pt.getX()));
+            t.rpush("y", String.valueOf(pt.getY()));
+            luares = t.eval(luaScript.getBytes(),0,"0".getBytes());
+            res = t.exec();
         }
+        jedis.close();
         msgt.convertAndSend("/topic/newpoint."+numdibujo, pt);
+        if (((ArrayList) luares.get()).size()==2){
+            List<Point> points = new ArrayList<Point>();
+            for(int i=0;i<5;i++){
+                  points.add(new Point(Integer.parseInt( new String((byte[])((ArrayList)(((ArrayList)luares.get()).get(0))).get(i))),Integer.parseInt( new String((byte[])((ArrayList)(((ArrayList)luares.get()).get(1))).get(i)))));
+            }
+            msgt.convertAndSend("/topic/newpolygon."+numdibujo, points);
+        }
+
     }
 }
